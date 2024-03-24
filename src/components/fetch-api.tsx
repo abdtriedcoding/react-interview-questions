853cea89e405aff19845df4b348e02aaa639ce88
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-03-25 01:24:49 +0530

    added pagination

--- src/components/fetch-api.tsx
+++ src/components/fetch-api.tsx
@@ -2,11 +2,20 @@ import { useEffect, useState } from "react";
 
 export default function FetchAPI() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchData = async () => {
-    const responce = await fetch("https://randomuser.me/api");
-    const data = await responce.json();
-    setData(data.results);
+    const response = await fetch(`https://randomuser.me/api?page=${1}`);
+    const responce = await response.json();
+    setData(responce.results);
+    setPage((prev) => prev + 1);
+  };
+
+  const fetchMore = async () => {
+    const response = await fetch(`https://randomuser.me/api?page=${page}`);
+    const responce = await response.json();
+    setData((prev) => [...prev, ...responce.results]);
+    setPage((prev) => prev + 1);
   };
 
   useEffect(() => {
@@ -15,14 +24,15 @@ export default function FetchAPI() {
 
   return (
     <div>
-      {data.map((e: any) => {
+      {data.map((e: any, index) => {
         return (
-          <div className="flex space-x-2">
+          <div className="flex space-x-2" key={index}>
             <p>{e.name.first}</p>
             <p>{e.gender}</p>
           </div>
         );
       })}
+      <button onClick={fetchMore}>Load More</button>
     </div>
   );
 }
